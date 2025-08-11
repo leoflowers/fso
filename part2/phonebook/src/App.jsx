d569356f30fc62e73058a023c35f295645f2c8d2
Leo Flores <leodflowers@gmail.com>
2025-08-11 12:29:46 -0400

    complete 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import Persons from './components/Persons'
@@ -6,12 +7,13 @@ import PersonForm from './components/PersonForm'
 
 const App = () => {
   // Existing contacts
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response => {
+      setPersons(response.data)
+    }))
+  })
 
   // Filtering information
   const [nameFilter, setNameFilter] = useState('')
